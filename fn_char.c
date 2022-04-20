303a7603668b72c8db87c2a096207837bfab9f5e
PatUdoh <udohpatience53@gmail.com>
2022-04-20 04:55:51 -0700

    update

--- fn_char.c
+++ fn_char.c
@@ -78,3 +78,36 @@ int print_S(va_list S)
 		}
 	}
 	return (count);
+}
+/**
+* print_rot - prints the rot13'ed string
+* @ro: the arguments list
+* Return: number of printed characters
+**/
+int print_rot(va_list ro)
+{
+	char *s = va_arg(ro, char *);
+	int i, j, count = 0;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while ((alpha[j] != '\0') && (s[i] != alpha[j]))
+		{
+			j++;
+		}
+		if (s[i] == alpha[j])
+		{
+			_putchar(rot[j]);
+			count++;
+		}
+		else if (alpha[j] == '\0')
+		{
+			_putchar(s[i]);
+			count++;
+		}
+	}
+	return (count);
+}
