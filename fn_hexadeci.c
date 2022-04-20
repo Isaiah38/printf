101d2b70157caa0b28e77e4b679cd7e13dfa1e58
Isaiah38 <brightlight882@gmail.com>
2022-04-20 05:56:15 -0700

    update

--- fn_hexadeci.c
+++ fn_hexadeci.c
@@ -66,3 +66,70 @@ int _print_o(va_list o)
 	}
 	return (count);
 }
+}
+/**
+* _print_x - prints input integer to an hexadecimal in lowercase
+* @x: the list of arguments
+* Return: number of printed characters
+*/
+int _print_x(va_list x)
+{
+	char hex[20];
+	unsigned int count = 0, n = 0;
+	int i = 0;
+
+	n = va_arg(x, unsigned int);
+	if (n == 0)
+	{
+		_putchar(0 + '0');
+		count++;
+	}
+	while (n > 0)
+	{
+		if (n % 16 >= 10 && n % 16 <= 15)
+			hex[i] = 87 + (n % 16);
+		else
+			hex[i] = 48 + (n % 16);
+		n = n / 16;
+		i++;
+	}
+	for (--i; i >= 0; i--)
+	{
+		_putchar(hex[i]);
+		count++;
+	}
+	return (count);
+}
+/**
+* _print_X - prints input integer to an hexadecimal in uppercase
+* @X: the list of arguments
+* Return: number of printed characters
+*/
+int _print_X(va_list X)
+{
+	char hex[20];
+	unsigned int count = 0, n = 0;
+	int i = 0;
+
+	n = va_arg(X, unsigned int);
+	if (n == 0)
+	{
+		_putchar(0 + '0');
+		count++;
+	}
+	while (n > 0)
+	{
+		if (n % 16 >= 10 && n % 16 <= 15)
+			hex[i] = 55 + (n % 16);
+		else
+			hex[i] = 48 + (n % 16);
+		n = n / 16;
+		i++;
+	}
+	for (--i; i >= 0; i--)
+	{
+		_putchar(hex[i]);
+		count++;
+	}
+	return (count);
+}
